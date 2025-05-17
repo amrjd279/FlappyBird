63d7d14a5fa623374a6bf44dc142e107524b73d8
amrjd279 <160243803+amrjd279@users.noreply.github.com>
2025-05-17 15:40:37 +0200

    Correction
    De faute orthographes

--- game.py
+++ game.py
@@ -6,10 +6,10 @@ pygame.font.init()
 class GameIndicator:
     def __init__(self, screen):
         self.screen = screen
-        self.font = pygame.font.SysFont("Bauhaus 93", 60)
-        self.inst_font = pygame.font.SysFont("Bauhaus 93", 30)
-        self.color = pygame.Color("White")
-        self.inst_color = pygame.Color("Black")
+        self.font = pygame.font.SysFont('Bauhaus 93', 60)
+        self.inst_font = pygame.font.SysFont('Bauhaus 93', 30)
+        self.color = pygame.Color("white")
+        self.inst_color = pygame.Color("black")
 
     def show_score(self, int_score):
         bird_score = str(int_score)
@@ -17,9 +17,9 @@ class GameIndicator:
         self.screen.blit(score, (WIDTH // 2, 50))
 
     def instructions(self):
-        inst_text1 = "Appuyez sur la touche ESPACE pour sauter,"
-        inst_text2 = "Appuyez sur la touche R pour redémarrer."
+        inst_text1 = "Press SPACE button to Jump,"
+        inst_text2 = "Press \"R\" Button to Restart Game."
         ins1 = self.inst_font.render(inst_text1, True, self.inst_color)
         ins2 = self.inst_font.render(inst_text2, True, self.inst_color)
-        self.screen.blit(ins1, 95, 400))
-        self.screen.blit(ins2, 70, 450))
+        self.screen.blit(ins1, (95, 400))
+        self.screen.blit(ins2, (70, 450))
