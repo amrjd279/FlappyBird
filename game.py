6d9848dffd883557d15d453520611c65ebf8bf2b
amrjd279 <160243803+amrjd279@users.noreply.github.com>
2025-05-17 13:28:45 +0200

    Deuxième méthode
    Dans cette show_score()fonction, le score du joueur est converti en chaîne de caractères, puis affiché avec la police et la couleur spécifiées. Le texte du score obtenu est ensuite placé à l'écran, horizontalement au centre, à 50 pixels du haut

--- game.py
+++ game.py
@@ -10,3 +10,8 @@ class GameIndicator:
         self.inst_font = pygame.font.SysFont("Bauhaus 93", 30)
         self.color = pygame.Color("White")
         self.inst_color = pygame.Color("Black")
+
+    def show_score(self, int_score):
+        bird_score = str(int_score)
+        score = self.font.render(bird_score, True, self.color)
+        self.screen.blit(score, (WIDTH // 2, 50))
