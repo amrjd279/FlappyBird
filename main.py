aa5f84dcb4e307335128de1c48112ab7fef1ab2f
amrjd279 <160243803+amrjd279@users.noreply.github.com>
2025-05-17 15:57:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
+# main.py
 import pygame, sys
 from settings import WIDTH, HEIGHT, ground_space
 from world import World
 
 pygame.init()
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Flappy Bird | 2024 - 2025")
+screen = pygame.display.set_mode((WIDTH, HEIGHT + ground_space))
+pygame.display.set_caption("Flappy Bird")
 
 class Main:
     def __init__(self, screen):
         self.screen = screen
-        self.bg_img = pygame.image.load("assets/terrain/bg.png")
+        self.bg_img = pygame.image.load('assets/terrain/bg.png')
         self.bg_img = pygame.transform.scale(self.bg_img, (WIDTH, HEIGHT))
-        self.ground_img = pygame.image.load("assets/terrain/ground.png")
+        self.ground_img = pygame.image.load('assets/terrain/ground.png')
         self.ground_scroll = 0
         self.scroll_speed = -6
         self.FPS = pygame.time.Clock()
         self.stop_ground_scroll = False
-    
+
     def main(self):
         world = World(screen)
-        running = True
-        while running:
+        while True:
             self.stop_ground_scroll = world.game_over
             self.screen.blit(self.bg_img, (0, 0))
             for event in pygame.event.get():
