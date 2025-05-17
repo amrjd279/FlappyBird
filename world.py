95a75583ab2e1224f9f5bcd73f847fd7f68333c3
amrjd279 <160243803+amrjd279@users.noreply.github.com>
2025-05-17 10:29:13 +0200

    Une casse
    Ajout d'une classe de world, qui est les attributs en public et protégé pour (self._generate_world()) --> la _generate_world()fonction sera appelée qui génère le monde du jeu

--- world.py
+++ world.py
@@ -4,3 +4,18 @@ from bird import Bird
 from game import GameIndicator
 from settings import WIDTH, HEIGHT, pipe_size, pipe_gap, pipe_pair_sizes
 import random
+
+class World:
+    def __init__(self, screen):
+        self.screen = screen
+        self.world_shift = 0
+        self.current_x = 0
+        self.gravity = 0.5
+        self.current_pipe = None
+        self.pipes = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
+        self._generate_world()
+        self.playing = False
+        self.game_over = False
+        self.passed = True
+        self.game = GameIndicator(self.screen)
