943c85c6459b6ed9892cc2877339a43254b30ae8
amrjd279 <160243803+amrjd279@users.noreply.github.com>
2025-05-17 10:03:58 +0200

    La taille + Le nom
    Ajout de la taille de la fenêtre grâce avec une variable "screen"
    Et ajout d'un nom a la fenêtre qui est "Flappy Bird | 2024 - 2025"

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ from settings import WIDTH, HEIGHT, ground_space
 from world import World
 
 pygame.init()
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Flappy Bird | 2024 - 2025")
