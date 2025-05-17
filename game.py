1d033feaa3849aa69d662cd19d1d484d6af90b5e
amrjd279 <160243803+amrjd279@users.noreply.github.com>
2025-05-17 13:21:15 +0200

    Première méthode
    Les couleurs (blanc et noir) de la police
    La famille (Bauhaus 93) de la police
    La taille (60 et 30) de la police

--- game.py
+++ game.py
@@ -2,3 +2,11 @@ import pygame
 from settings import WIDTH, HEIGHT
 
 pygame.font.init()
+
+class GameIndicator:
+    def __init__(self, screen):
+        self.screen = screen
+        self.font = pygame.font.SysFont("Bauhaus 93", 60)
+        self.inst_font = pygame.font.SysFont("Bauhaus 93", 30)
+        self.color = pygame.Color("White")
+        self.inst_color = pygame.Color("Black")
