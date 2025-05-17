efc5ab91c9cac2e9ae5ce477b9ec3b1cc3226034
amrjd279 <160243803+amrjd279@users.noreply.github.com>
2025-05-17 09:51:24 +0200

    Le graphisme
    Ajouts la taille de l'écran (en px) et l'ajout la taille des tuyaux.

--- settings.py
+++ settings.py
@@ -1,2 +1,17 @@
 from os import walk
 import pygame
+
+WIDTH, HEIGHT = 600, 650
+
+pipe_pair_sizes = [
+    (1,7),
+    (2,6),
+    (3,5),
+    (4,4),
+    (5,3),
+    (6,2),
+    (7,1),
+]
+pipe_size = HEIGHT // 10
+pipe_gap = (pipe_size * 2) + (pipe_size // 2)
+ground_space = 50
